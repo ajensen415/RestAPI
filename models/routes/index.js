eba6761cb4103deda62dacc2012596e2a3732616
ajensen415 <ajensen415@yahoo.com>
2021-09-05 18:28:00 -0400

    Step 7 - course routes

--- models/routes/index.js
+++ models/routes/index.js
@@ -49,11 +49,71 @@ router.post('/users', asyncHandler(async (req, res) => {
 /*Course Routes*/
 
 // GET route to return all courses 
+router.get('/courses', asyncHandler(async (req, res) => {
+    const courses = await Course.findAll({
+        attributes: { exclude: ['createdAt', 'updatedAt'] },
+        include: {
+            model: User,
+            attributes: { exclude: ['password', 'createdAt', 'updatedAt'] },
+        },
+    });
+    res.status(200).json({ courses });
+}));
 
 // GET route to return corresponding course & User
+router.get('/courses/:id', asyncHandler(async (req, res) => {
+    const course = await Course.findOne({
+        attributes: { exclude: ['createdAt', 'updatedAt'] },
+        where: { id: req.params.id },
+        include: {
+            model: User,
+            attributes: { exclude: ['password', 'createdAt', 'updatedAt'] }
+        },
+    });
+    res.status(200).json({ course });
+}));
 
 // POST route to create a new course 
+router.post('/courses', asyncHandler(async (req, res) => {
+    const newCourse = await Course.create(req.body);
+    res.status(201).location(`/courses/${newCourse.id}`).end();
+}));
+
 
 // PUT route to update course
+router.put('/courses/:id', asyncHandler(async (req, res) => {
+    const course = await Course.findByPk(req.params.id);
+
+    if (course) {
+        if (course.userId === req.currentUser.id) {
+            awaitcourse.update({
+                title: req.body.title,
+                description: req.body.description,
+            });
+            res.status(204).end();
+        } else {
+            res.status(403).end();
+        }
+    } else {
+        res.status(404).json({ message: 'Unable to locate course. Please try again.' });
+    }
+}));
 
 // Delete route to delete course
+router.delete('/courses/:id', asyncHandler(async (req, res) => {
+    const course = await Course.findByPk(req.params.id);
+
+    if (course) {
+        if (course.userId === course.currentUser.id) {
+            await course.destroy(course);
+            res.status(204).end();
+        } else {
+            res.status(403).end();
+        }
+    } else {
+        res.status(404).json({ message: 'Unable to locate course. Please try again.'});
+    }
+}));
+
+module.exports = router;
+
